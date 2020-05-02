214c22b3cc9e34b62ce3aaa2be70f0c5122f57d7
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-05-02 17:03:28 +0530

    fi

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -286,4 +286,8 @@ public class CharacterController2D : MonoBehaviour {
 		_transform.position = spawnloc;
 		_animator.SetTrigger("Respawn");
 	}
+	public void EnemyBounce()
+	{
+		DoJump();
+	}
 }
